439b004e1ae93ef52eed0d23244a11a2b80a8920
Yadwinder Singh <ryker1990@gmail.com>
2016-04-22 17:24:22 +0530

    Update TSL2569.c

--- C/TSL2569.c
+++ C/TSL2569.c
@@ -1,4 +1,4 @@
-// Distributed with a free-will license
+// Distributed with a free-will license.
 // Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
 // TSL2569
 // This code is designed to work with the TSL2569_I2CS I2C Mini Module available from ControlEverything.com.
